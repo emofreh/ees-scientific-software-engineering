b6c7e05bcf88fca0398a299adb2c2378d76dd258
akhtarhussainjaved@gmail.com <akhtar3100@TUE025111.campus.tue.nl>
2024-12-04 15:06:15 +0100

    Akhtar RMS

--- tests/test_simple_function.py
+++ tests/test_simple_function.py
@@ -1,5 +1,5 @@
+import numpy as np
 import pytest
-import numpy
 
 from ees_scientific_software_engineering.simple_function import add, multiply, rms
 
@@ -18,8 +18,9 @@ def test_add_error():
     with pytest.raises(TypeError, match="Arguments should be integers!"):
         add(a, b)
 
+
 def test_rms():
     # Test case 1: Input array of integers
     input_array = np.array([5, 4, 3])
     expected_result = np.sqrt((5**2 + 4**2 + 3**2) / 3)  # RMS formula
-    assert np.isclose(rms(input_array), expected_result), 
+    assert np.isclose(rms(input_array), expected_result)
